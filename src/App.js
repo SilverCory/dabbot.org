f85ebdbcc843a9fe992c57134a02c7d9a208a1a8
SpongyBacon <spongybacon@hotmail.co.uk>
2017-10-12 18:09:45 +0100

    initial rewrite

--- src/App.js
+++ src/App.js
@@ -1,20 +1,98 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container />
     );
+  }
+}
+
+class Container extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="masthead">
+          <Navbar />
+          <Jumbotron />
+        </div>
+      </div>
+    )
+  }
+}
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar nav-expand-md navbar-light bg-light rounded mb-3">
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="navbar-nav text-md-center nav-justified w-100">
+            <NavbarItem />
+            <NavbarItem />
+            <NavbarItem />
+            <NavbarItem />
+            <NavbarItem />
+            <NavbarItem />
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
+
+class NavbarItem extends Component {
+  render() {
+    return (
+      <li className="nav-item">
+        <a className="nav-link" href="#">
+          Home
+        </a>
+      </li>
+    )
+  }
+}
+
+class Jumbotron extends Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <JumbotronHeader />
+        <JumbotronLead />
+        <JumbotronButton />
+      </div>
+    )
+  }
+}
+
+class JumbotronHeader extends Component {
+  render() {
+    return (
+      <h1>dabBot - the music bot that makes you dab</h1>
+    )
+  }
+}
+
+class JumbotronLead extends Component {
+  render() {
+    return (
+      <p className="lead">
+        A Discord music bot providing high quality music from YouTube, Radio Stations, Soundcloud, Twitch, Discord.FM and much more.
+      </p>
+    )
+  }
+}
+
+class JumbotronButton extends Component {
+  render() {
+    return (
+      <p>
+        <a className="btn btn-lg btn-success" href="https://dabbot.org/invite" role="button">
+          Invite dabBot
+        </a>
+      </p>
+    )
   }
 }
 
