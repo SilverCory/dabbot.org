2ded8ff82f600da5f22ed4bd7478353f82b25672
SpongyBacon <spongybacon@hotmail.co.uk>
2017-10-19 14:15:10 +0100

    replace apostrophe with html entity

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ class Footer extends Component {
           <div className="content has-text-centered">
             <p>
               <strong className="footer-em-text">DAB Bot</strong> by <a href="https://github.com/orgs/dabbotorg/people" className="has-text-success">DAB Bot Development Team</a>.
-              This website's content is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/" className="has-text-success">CC BY NC SA 4.0</a>.
+              This website&apos;s content is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/" className="has-text-success">CC BY NC SA 4.0</a>.
             </p>
           </div>
         </div>
