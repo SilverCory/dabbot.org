461adb3eb52624d4cdd5986a475e89ff0820b3b1
Cory Ory <ace@ac3-servers.eu>
2018-01-04 20:33:31 +0000

    add dabbot logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ export default class Home extends Component {
 	render() {
 		return (
 			<div>
-				<h4>hello dabbers</h4>
+				<h4>hello dabbers<br/><img src="https://avatars3.githubusercontent.com/u/31905519" alt="logo"/></h4>
 			</div>
 		);
 	}
